c89f5b232004a449b366b24d691bf9356cf9cef9
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-29 10:19:53 -0300

    Integrando Prisma no Projeto

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.get("/test", (req, res) => {
+    prisma.user
+})
